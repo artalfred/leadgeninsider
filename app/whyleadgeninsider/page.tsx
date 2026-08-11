75cd5045a0d8a2198451fceea4b3ba860191d79f
Art Alfred <bernalesartalfred@gmail.com>
2026-08-12 05:59:36 +0800

    New Update 8/12/2026

--- app/whyleadgeninsider/page.tsx
+++ app/whyleadgeninsider/page.tsx
@@ -205,7 +205,6 @@ export default function whyleadgeninsider() {
                 title="Send us a message – we reply in minutes."
                 style="primary-btn mt-4"
                 link="/contact"
-                align=""
               ></Button>
             </div>
           </div>
